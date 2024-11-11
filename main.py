71b0c9a2f398f31a05a7a5d5f4034294a3448a2a
geotilageamb <geoti.lageamb@ufpr.br>
2024-11-11 15:25:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Função para carregar dados de uma aba específica do Excel
 def load_data(sheet_name):
-    return pd.read_excel('indicadoresGeoTI_11nov2024.xlsx', sheet_name=sheet_name)
+    return pd.read_excel('indicadoresGeoTI_11nov2024.xlsx', sheet_name=sheet_name, header=1)
 
 # Títulos das abas que correspondem aos dashboards
 sheet_names = ['Planilha1', 'Planilha2', 'Planilha3', 'Planilha4']
@@ -23,36 +23,24 @@ tab1, tab2, tab3, tab4 = st.tabs([
 with tab1:
     st.header('Chamados GeoTI por Categoria (11/11/2024)')
     data1 = load_data(sheet_names[0])
-    if 'Categoria' in data1.columns:
-        st.bar_chart(data1.set_index('Categoria'))
-    else:
-        st.write("Coluna 'Categoria' não encontrada.")
+    st.bar_chart(data1.set_index('Categoria'))
 
 # Dashboard 2
 with tab2:
     st.header('Chamados GeoTI por Status (11/11/2024)')
     data2 = load_data(sheet_names[1])
-    if 'Status' in data2.columns:
-        st.bar_chart(data2.set_index('Status'))
-    else:
-        st.write("Coluna 'Status' não encontrada.")
+    st.bar_chart(data2.set_index('Status'))
 
 # Dashboard 3
 with tab3:
     st.header('Chamados GeoTI por Mês (01/05/2023-31/12/2023)')
     data3 = load_data(sheet_names[2])
-    if 'Mês' in data3.columns:
-        st.line_chart(data3.set_index('Mês'))
-    else:
-        st.write("Coluna 'Mês' não encontrada.")
+    st.line_chart(data3.set_index('Mês'))
 
 # Dashboard 4
 with tab4:
     st.header('Chamados GeoTI por Mês (01/01/2024-11/11/2024)')
     data4 = load_data(sheet_names[3])
-    if 'Mês' in data4.columns:
-        st.line_chart(data4.set_index('Mês'))
-    else:
-        st.write("Coluna 'Mês' não encontrada.")
+    st.line_chart(data4.set_index('Mês'))
 
 # Para executar o aplicativo, use o comando: streamlit run nome_do_arquivo.py
